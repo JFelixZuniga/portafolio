af72f570995e24565ca97483f4b3490ff431f8c6
JFelixZuniga <johnfzuniga@gmail.com>
2020-11-11 23:07:05 -0300

    11 - animación de           <div class=about-tabs> main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-
-
 /*--------------- About section tabs ---------------*/
 //invocamos de inmediato la función
 (() => {
+  const aboutSection = document.querySelector(".about-section"),
+    tabsContainer = document.querySelector(".about-tabs");
 
-})
+  tabsContainer.addEventListener("click", (event) => {
+    //si cotiene la clase "tab-item" y no contiene la clase "active"
+    if (
+      event.target.classList.contains("tab-item") &&
+      !event.target.classList.contains("active")
+    ) {
+      const target = event.target.getAttribute("data-target");
+      //desactivamos si existe active "tab-item"
+      tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+      //acivamos un nuevo "tab-item"
+      event.target.classList.add("active", "outer-shadow")
+      //desactivamos si existe active "tab-item"
+      aboutSection.querySelector(".tab-content.active").classList.remove("active");
+      //acivamos un nuevo "tab-item"
+      aboutSection.querySelector(target).classList.add("active")
+    }
+  });
+})();
