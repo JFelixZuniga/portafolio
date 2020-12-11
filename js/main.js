41ce74b162ede62fb6d190708155fbd4ae3f6b7a
JFelixZuniga <johnfzuniga@gmail.com>
2020-12-10 22:17:13 -0300

    29 - preloader listo

--- js/main.js
+++ js/main.js
@@ -316,3 +316,12 @@ function bodyScrollingToggle() {
     }
   });
 })();
+
+
+window.addEventListener("load", () => {
+  //Preloader
+  document.querySelector(".preloader").classList.add("fade-out")
+  setTimeout(() => {
+    document.querySelector(".preloader").style.display = "none"
+  }, 600)
+})
